e50dbc257a53691bfe85c7a1d47cef2b990f0a03
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-03-27 19:09:02 -0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '81676888-2c32-49c9-99aa-ab14c7eb4cc0';
+export const MODAL_CLOSE_SEC = 2.5;
